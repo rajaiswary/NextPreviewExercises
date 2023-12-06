ca469506551107ecc4b9b1a8fa7115db127afbba
rajaiswary <rajaiswary502@gmail.com>
2023-12-06 12:20:23 +0530

    implemented live preview with CSR but with error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+'use client';
+import type { AppProps } from "next/app";
+import '@contentful/live-preview/style.css';
+import { ContentfulLivePreviewProvider } from "@contentful/live-preview/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+  <ContentfulLivePreviewProvider
+    locale="en-US"
+    enableInspectorMode={true}
+    enableLiveUpdates={true}
+  >
+    <Component {...pageProps} />
+  </ContentfulLivePreviewProvider>
+  )
 }
