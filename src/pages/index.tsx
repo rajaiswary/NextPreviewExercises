532bfc35e4316ae324ded15857098bfb1302a691
rajaiswary <rajaiswary502@gmail.com>
2024-01-08 21:13:46 +0530

    env updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,14 +31,15 @@ export default function Home({ responseData } : any) {
 export async function getServerSideProps({preview}: any) {
   console.log("context: " + preview);
   
-  const accessToken = "xvbvMYmZ_i9tUa5x1Iv_mGS5xoLUCMpzebJkOY84t6c";
-  const previewToken = "X6C53ERCncqkVmmzdqJphw8hAh6IPvzKE7YrMHicP_8";
+  const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN as string;
+  const previewToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN as string;
 
   const client = createClient({
-    space: "lg78ad1ah3gx",
+    space: process.env.CONTENTFUL_SPACE_ID as string,
     accessToken: preview ? previewToken : accessToken,
     host: preview ? 'preview.contentful.com' : undefined,
   });
+ 
   const response = await client?.getEntries({
     content_type: "sampleHeading",
   });
